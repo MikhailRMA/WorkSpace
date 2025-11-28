51d5822e81065a9e273fae1a774cedbe171e977a
Михаил <majkl.a@yandex.ru>
2025-11-28 14:29:21 +0000

    modified:   Brand_Detector/app.py

--- Brand_Detector/app.py
+++ Brand_Detector/app.py
@@ -384,6 +384,29 @@ def show_example_section():
         """)
 
 def main_page():
+
+ # Яндекс.Метрика
+    metrika_code = """
+    <script>
+        (function(m,e,t,r,i,k,a){m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
+        m[i].l=1*new Date();
+        k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)})
+        (window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym");
+    
+        ym(105560408, "init", {
+            clickmap:true,
+            trackLinks:true,
+            accurateTrackBounce:true,
+            webvisor:true
+        });
+     
+    </script>
+    <noscript><div><img src="https://mc.yandex.ru/watch/105560408" style="position:absolute; left:-9999px;" alt="" /></div></noscript>
+    """
+    
+    st.markdown(metrika_code, unsafe_allow_html=True)
+
+
     """Главная страница - загрузка и обработка данных"""
     
     # Заголовок и инструкции
